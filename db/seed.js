e81d51d5d6269131721bd5e5f716a7e06132b649
Merlin Valdez <163349473+merlinvaldez@users.noreply.github.com>
2025-11-03 17:02:48 -0500

    test is passing

--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,6 @@
 import db from "#db/client";
+import { faker } from "@faker-js/faker";
+import { createEmployee } from "./queries/employees.js";
 
 await db.connect();
 await seedEmployees();
@@ -7,4 +9,12 @@ console.log("🌱 Database seeded.");
 
 async function seedEmployees() {
   // TODO
+  for (let i = 0; i < 10; i++) {
+    const employee = {
+      name: faker.person.fullName(),
+      birthday: faker.date.birthdate(),
+      salary: faker.number.int({ max: 10000 }),
+    };
+    await createEmployee(employee);
+  }
 }
