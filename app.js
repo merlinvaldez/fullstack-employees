6f1f942ead641ff8b470883603a1eda3e7fd3d6d
Merlin Valdez <163349473+merlinvaldez@users.noreply.github.com>
2025-11-05 18:14:05 -0500

    db test passing

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 import express from "express";
 const app = express();
 export default app;
+import router from "#api/employees";
 
 // TODO: this file!
+app.use(express.json());
+
+app.use("/", router);
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send("sorry! Something Went Wrong");
+});
